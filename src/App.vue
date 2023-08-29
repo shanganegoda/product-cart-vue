2e50605ce8b1b0289ca346f55853c15aa6f5e4a7
shanganegoda <106309376+shanganegoda@users.noreply.github.com>
2023-08-29 10:25:32 +0800

    Hookes up pages with Sidebar and Product Card (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,74 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <header class="top-bar spread">
+      <nav class="top-bar-nav">
+        <router-link to="/" class="top-bar-link">
+          <i class="icofont-spoon-and-fork"></i>
+          <span>Home</span>
+        </router-link>
+        <router-link to="/products" class="top-bar-link">
+          <span>Products</span>
+        </router-link>
+        <router-link to="/past-orders" class="top-bar-link">
+          <span>Past Orders</span>
+        </router-link>
+      </nav>
+      <a @click="toggleSideBar" class="top-bar-cart-link">
+        <i class="icofont-cart-alt icofont-1x"></i>
+        <span>Cart ({{ totalQuantity }})</span>
+      </a>
+    </header>
+  <router-view :inventory="inventory" :addToCart="addToCart" />
+   
+   
+  <Sidebar
+  v-if="showSideBar" :toggle="toggleSideBar" :cart="cart" :inventory="inventory" :remove="removeItem"
+  />
+  
 </template>
+
+<script>
+import Sidebar from '@/components/Sidebar.vue'
+import food from './food.json'
+// @ is an alias to /src
+export default {
+  components: {Sidebar},
+  data () {
+    return {
+      inventory: food,
+      showSideBar: false,
+      cart: {}
+    }
+  },
+  methods: {
+        addToCart(i, name) {
+          if (!this.cart[name]) {
+            this.cart[name] = 0;
+          }
+          console.log(this.cart)
+          this.cart[name] += this.inventory[i].quantity
+          //console.log(JSON.stringify(this.cart))
+          this.inventory[i].quantity = 0;
+        },
+
+        toggleSideBar() {
+          this.showSideBar = !this.showSideBar
+        },
+
+        removeItem(name) {
+          delete this.cart[name];
+
+        }
+      },
+
+      computed: {
+        totalQuantity() {
+          return Object.values(this.cart).reduce((total, current) => {
+            return total += current;
+          }, 0)
+        }
+      }
+}
+</script>
 
 <style>
 #app {
